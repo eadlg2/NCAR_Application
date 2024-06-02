2c462f6d6549b7f6e3c6b2a771adebb39906f344
Evan De la Garza <edelagarza2002@gmail.com>
2024-06-02 16:02:40 -0500

    add command line interface

--- src/foo_parameterization/foo.py
+++ src/foo_parameterization/foo.py
@@ -14,3 +14,22 @@ def foo(radius):
         raise ValueError("Radius cannot be negative")
 
     return (4/3) * math.pi * radius**3
+
+if __name__ == "__main__":
+    while True:
+        radius = input("Enter the radius of the sphere ('q' to quit): ")
+
+        if (radius == "q"):
+            break
+        
+        try:
+            radius = float(radius)
+        except ValueError:
+            print("Please enter a valid number")
+            continue
+
+        try:
+            volume = foo(radius)
+            print(f"\nThe volume of the sphere is {volume}\n")
+        except ValueError as e:
+            print(e)
